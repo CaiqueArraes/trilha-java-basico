77b630ab18400eba1b2b41a35458258554433fac
Caique Arraes <caiquearraes@gmail.com>
2024-07-24 21:27:54 -0300

    atualizacoes

--- controle-fluxo/src/estruturas-condicional/ResultadoEscolar.java
+++ controle-fluxo/src/estruturas-condicional/ResultadoEscolar.java
@@ -1,7 +1,7 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
 
-        int nota = 7;
+        int nota = 6;
         
         if(nota >= 7)
          System.out.println("Aprovado");
